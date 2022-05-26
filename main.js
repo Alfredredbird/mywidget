55fb024281e5cd4b79267162e16a49ec0303efd0
alfredredbird <105014217+Alfredredbird@users.noreply.github.com>
2022-05-26 10:30:39 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+function time(){
+var d = new Date();
+var hours = d.getHours();
+var mins = d.getMinutes();
+var secs = d.getSeconds();
+
+document.getElementById("time").innerHTML = hours + ":" + mins + ":" + secs;
+}
+
+setInterval(time);
